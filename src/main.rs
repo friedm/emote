e4e1c06810931681d48d39a6c8c4ae2677c2a2e8
Matt Friedman <matt@friedm.com>
2018-06-08 01:18:05 -0700

    working lookup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate toml;
 extern crate clap;
 
+use std::process::exit;
+use std::collections::HashMap;
+
 use clap::{Arg, App};
 use toml::Value;
 
@@ -11,6 +14,8 @@ const AMOJI_TOML: &'static str = include_str!("amoji.toml");
 
 fn main() {
     let value = AMOJI_TOML.parse::<Value>().expect("invalid toml in amoji.toml!");
+    let amoji_map = build_amoji_map(&value);
+    // TODO avoid loading the entire map on every invocation -- cache serialized HashMap?
 
     let cli_matches = App::new(NAME)
                           .version(VERSION)
@@ -20,9 +25,39 @@ fn main() {
                           .arg(Arg::with_name("text")
                                .required(true)
                                .index(1))
+                          // TODO list subcommand
+                          // TODO bash completion (gen_completions)
                           .get_matches();
     let use_clipboard = cli_matches.is_present("clipboard");
-    let input = cli_matches.value_of("text").unwrap();
+    let text = cli_matches.value_of("text").unwrap();
+    match amoji_map.get(text) {
+        Some(amoji) => println!("{}", amoji),
+        None => {
+            println!("no match for {}", text);
+            exit(1);
+        }
+    }
+}
 
-    println!("{:?} {:?}", use_clipboard, input);
+fn build_amoji_map<'a>(toml_value: &'a Value) -> HashMap<&'a str, &'a str> {
+    let mut map = HashMap::new();
+
+    for (_, item) in toml_value["amoji"].as_table()
+                                        .expect("unexpected amoji toml!")
+                                        .into_iter() {
+        let item = item.as_table()
+                       .expect(&format!("unexpected amoji toml: {:?}", item));
+        let amoji = item["amoji"].as_str()
+                                 .expect(&format!("missing amoji in toml: {:?}", item))
+                                 .clone();
+        let words = item["words"].as_array()
+                                 .expect(&format!("missing words in toml: {:?}", item));
+        for word in words {
+            let word = word.as_str()
+                           .expect(&format!("missing word in toml: {:?}", item));
+            map.insert(word, amoji);
+        }
+    }
+
+    map
 }
